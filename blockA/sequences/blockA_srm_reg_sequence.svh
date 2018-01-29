5edac60bc2092193f573a01ac1d5aafa990ee878
Sanjeev Singh <jscott@juniper.net>
2018-01-29 12:20:29 -0800

    fixed reg seq

--- blockA/sequences/blockA_srm_reg_sequence.svh
+++ blockA/sequences/blockA_srm_reg_sequence.svh
@@ -7,6 +7,7 @@ class blockA_srm_reg_sequence extends uvm_sequence;
   BlockA regmodel;
   srm_base_handle handle;
   reg[31:0] rd_data;
+  reg[31:0] exp_data;
 
   function new(string name="");
     super.new(name);
@@ -18,20 +19,21 @@ class blockA_srm_reg_sequence extends uvm_sequence;
   endfunction
 
   virtual task body();
-    `uvm_info(get_full_name(), "Starting blockA_srm_reg after reset", UVM_LOW);
-    regmodel.r1_reg_inst.write(handle, 'hdeadbeef);
-    `uvm_info(get_full_name(), "Finished blockA_srm_reg write", UVM_LOW);
+    `uvm_info(get_full_name(), "Test Wr-Rd 0xdeadbeef to r1_reg", UVM_LOW);
+    exp_data = 32'hdeadbeef;
+    regmodel.r1_reg_inst.write(handle, exp_data);
 
     `uvm_info(get_full_name(), "Starting blockA_srm_reg read", UVM_LOW);
     regmodel.r1_reg_inst.read(handle, rd_data);
-    `uvm_info(get_full_name(), "Finished blockA_srm_reg read", UVM_LOW);
-    `uvm_info(get_full_name(), 
-      $psprintf("Read-Write register ReadData=0x%0x", rd_data), UVM_LOW);
-    
-    regmodel.r1_reg_inst.write(handle, 'hf00dcafe);
-    regmodel.r1_reg_inst.read(handle, rd_data);
-    `uvm_info(get_full_name(), 
-      $psprintf("Read-Write register ReadData=0x%0x", rd_data), UVM_LOW);
+    if(rd_data != exp_data) begin
+      `uvm_error(get_full_name(), 
+        $psprintf("Read Mismatch. Reg r1 ReadData=0x%0x, Exp=0x%0x", rd_data, exp_data))
+    end
+    else begin
+      `uvm_info(get_full_name(), 
+        $psprintf("Reg Read Match. Reg r1 ReadData=0x%0x, Exp=0x%0x", rd_data, exp_data),
+        UVM_LOW);
+    end
   endtask
 endclass
 
